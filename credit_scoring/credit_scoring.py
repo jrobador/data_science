0af2ecc6b785de251b671a48014ebbfa9caf9331
Joaquín Robador <joaquinrobador@gmail.com>
2024-10-07 22:25:48 -0300

    cs: code updated

--- credit_scoring/credit_scoring.py
+++ credit_scoring/credit_scoring.py
@@ -12,6 +12,16 @@ import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import RobustScaler, OneHotEncoder
 
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+from sklearn.manifold import TSNE
+
+from imblearn.over_sampling import SMOTE
+
+from collections import Counter
+
+from xgboost import XGBClassifier
+
+
 #%%
 # fetch dataset 
 statlog_german_credit_data = fetch_ucirepo(id=144) 
@@ -92,27 +102,131 @@ for feature in (categorical_features):
 # Handling multivariate features
 X.loc[:, continuous_features] = RobustScaler().fit_transform(X[continuous_features])
 
+encoder = OneHotEncoder(sparse_output=False, drop='first')  # drop='first' para evitar la multicolinealidad (one feature is a linear combination of others)
+encoded_categorical = encoder.fit_transform(X[categorical_features])
+
+encoded_X = pd.DataFrame(encoded_categorical, columns=encoder.get_feature_names_out(categorical_features))
+
+X = X.drop(columns=categorical_features).reset_index(drop=True)
+encoded_X = encoded_X.reset_index(drop=True)
+X = pd.concat([X, encoded_X], axis=1)
+
 #%%
 # Target distibution
+tg_dist = sns.countplot(data=df, x='class', hue='class', palette='cividis', legend=False)
+tg_dist.set_xticklabels(['Good', 'Bad'])
+
+total = sum([p.get_height() for p in tg_dist.patches])
+for p in tg_dist.patches:
+    height = p.get_height()
+    percentage = 100 * height / total
+    tg_dist.annotate(f'{percentage:.0f}%', (p.get_x() + p.get_width() / 2., p.get_height()), 
+                      ha='center', va='bottom', fontsize=12)
+    
+plt.tight_layout()
+
+if not os.path.exists('./credit_scoring/plots/y_dist_imbal'):
+    plt.savefig('./credit_scoring/plots/y_dist_imbal')
+plt.show()
 
 # Since we have an imbalanced dataset with few samples -> Oversampling
 #%%
 # Splitting data
-
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=37, stratify=y)
 #%%
 # Oversampling
+X_oversampled, y_oversampled = SMOTE(random_state=37).fit_resample(X_train, y_train)
+df_oversampled = pd.concat([X_oversampled,y_oversampled], axis=1)
+#%%
+# Data distribution after oversampling
+tg_dist_2 = sns.countplot(data=df_oversampled, x='class', hue='class', palette='cividis', legend=False)
+tg_dist_2.set_xticklabels(['Good','Bad'])
+
+total = sum([p.get_height() for p in tg_dist_2.patches])
+for p in tg_dist_2.patches:
+    height = p.get_height()
+    percentage = 100 * height / total
+    tg_dist_2.annotate(f'{percentage:.0f}%', (p.get_x() + p.get_width() / 2., p.get_height()), 
+                      ha='center', va='bottom', fontsize=12)
+    
+plt.tight_layout()
+
+if not os.path.exists('./credit_scoring/plots/y_dist_bal'):
+    plt.savefig('./credit_scoring/plots/y_dist_bal')
+plt.show()
 
 #%%
 # Correlation matrix
+correlation_matrix_l = df_oversampled.corr(method='pearson', min_periods=1, numeric_only=False)
+
+plt.figure(figsize=(12, 8))
+sns.heatmap(correlation_matrix_l, annot=False, square=True, cmap='cividis', cbar=True)
+plt.title('Correlation Matrix between Features and Target')
+if not os.path.exists('./credit_scoring/plots/correlation_matrix.png'):
+    plt.savefig('./credit_scoring/plots/correlation_matrix.png')
+plt.show()
 
 #%%
-# Feature selection (if needed)
+print("Positive Correlation between features and class (Higher than 0.2):")
+pos_cor_feat = []
+for feature in correlation_matrix_l.columns:
+    if feature != 'class' and correlation_matrix_l.loc[feature, 'class'] > 0.2:
+        print(f"{feature}: {correlation_matrix_l.loc[feature, 'class']:.2f}")
+        pos_cor_feat.append(feature)
 
+neg_cor_feat = []
+print("Negative Correlation between features and class (Lower than -0.2):")
+for feature in correlation_matrix_l.columns:
+    if feature != 'class' and correlation_matrix_l.loc[feature, 'class'] < -0.2:
+        print(f"{feature}: {correlation_matrix_l.loc[feature, 'class']:.2f}")
+        neg_cor_feat.append(feature)
+
+#%%
+# Correlation inspection
+
+sns.boxplot(x="class", y=pos_cor_feat[0], data=df_oversampled)
+plt.title(f'{pos_cor_feat[0]} vs Class Positive Correlation')
+plt.tight_layout()
+plt.show()
+
+f, axes = plt.subplots(ncols=len(neg_cor_feat), figsize=(30,15))
+
+for i, feature in enumerate(neg_cor_feat):
+    sns.boxplot(x="class", y=f"{feature}", data=df_oversampled, ax=axes[i])
+    axes[i].set_title(f'{feature} vs Class Negative Correlation')
+
+plt.tight_layout()
+plt.show()
+#%%
 #%%
 # Dimensionality Visualization
+#t-SNE
+data_embedded_TSNE = TSNE(n_components=3, random_state=37).fit_transform(X_oversampled)
 
 #%%
+fig = plt.figure(facecolor="white", constrained_layout=True)
+ax = fig.add_subplot(projection='3d')
+
+y_oversampled_flat = y_oversampled.values.ravel()
+
+ax.scatter(data_embedded_TSNE[(y_oversampled_flat == 1),0], data_embedded_TSNE[(y_oversampled_flat == 1),1], c='yellow',  label="Non Fraud")
+ax.scatter(data_embedded_TSNE[(y_oversampled_flat == 2),0], data_embedded_TSNE[(y_oversampled_flat == 2),1], c='blue',    label="Fraud")
+
+ax.legend()
+
+ax.set_title('t-SNE')
+ax.grid(True)
+
+plt.show()
+#%%
 # Classifier task
+XGB_model = XGBClassifier(random_state=37)
+
+#1 was good, 2 was bad.
+y_oversampled_target = y_oversampled['class'].map({1: 0, 2: 1})
+
+score = cross_val_score(XGB_model, X_oversampled, y_oversampled_target,
+                             cv=StratifiedKFold(n_splits=5, random_state=37, shuffle=True), scoring='accuracy')
 
 #%% 
 # GridSearch - RandomSearch
