7513d7ee6fd9b0dbbe9343b4005db2af98b297c8
Joaquín Robador <joaquinrobador@gmail.com>
2025-02-19 23:51:44 -0300

    Update regressor.py

--- MIT+SCIKITLEARN-fundamentals/dl/regressor.py
+++ MIT+SCIKITLEARN-fundamentals/dl/regressor.py
@@ -1,6 +1,4 @@
 #%%
-
-import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 from typing import List
@@ -37,7 +35,7 @@ if n_features == 2:
     plt.scatter(X,Y)
 
 
-def linear(inp, weights, bias):
+def linear_function(inp, weights, bias):
     #Input: (batch_size, input_dim)
     #Weights: (input_dim, output_dim)
     #Bias: (output_dim)
@@ -51,39 +49,67 @@ def linear(inp, weights, bias):
     y = inp @ weights
     #Suma de bias
     y += bias
-    
+
+    #La salida es (batch_size, output_dim). Olvidarse un poco del concepto del batch_size
     return y
 
 
 # %%
 # Create a simple NN Regression Network
 class RegressionModel():
-    def __init__(self, input_layer_dim: int = n_features, hidden_layers: List = [3,3], output_layer = 1):
+    def __init__(self, input_layer_dim: int = n_features, hidden_layers: List = [3,3], output_layer_dim = 1):
         self.input_layer = input_layer_dim
         self.hidden_layers = hidden_layers
-        self.output_layer = output_layer
+        self.output_layer_dim = output_layer_dim
 
+
+    def initialization(self):
         #Inicializacion de capas
         #Capas de entrada
-        self.input_weights = np.random.rand(n_features, self.hidden_layers[0])
+        self.input_weights = np.random.rand(self.input_layer, self.hidden_layers[0])
         self.input_bias = np.random.rand(self.hidden_layers[0])
 
         #Capas ocultas
         self.hidden_weights = []
         self.hidden_bias = []
 
-        for i in range(1, hidden_layers):
+        for i in range(1, len(self.hidden_layers)):
             self.hidden_weights.append(np.random.randn(self.hidden_layers[i-1], self.hidden_layers[i]))
-            self.hidden_bias.append()
-
+            self.hidden_bias.append(np.random.rand(self.hidden_layers[i]))
 
         #Capas de salida
+        self.output_weights = np.random.rand(self.hidden_layers[-1], self.output_layer_dim)
+        self.output_bias    = np.random.rand(self.output_layer_dim)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        #Propagacion hacia adelante
 
+        #Capa de entrada
+        # X -> (batch_size, features)
+        # input_linear -> (batch_size, hidden[0])
+        input_linear = linear_function(x, self.input_weights, self.input_bias)
 
-    def loss(self):
+        #Capas ocultas
+        # input_linear -> (batch_size, hidden[0])
+        # hidden_linear[n] -> (hidden[0], output_size)
+        linear_y = []
+        input_iter = input_linear
+        for i in range (1, len(self.hidden_layers)):
+            linear_y.append(linear_function(input_iter, self.hidden_weights[i], self.hidden_bias[i]))
+            input_iter = linear_y[-1]
+        
+        #Capas de salida
+        output_linear = linear_function(input_iter, self.output_weights, self.output_bias)
+
+        return output_linear
+
+    def loss(self, y, forward_output):
+        # Vamos a calcular la suma cuadrática media
+        MSE = np.mean((y - forward_output)**2)
+
+        return MSE
+
+    def backpropagation(self):
         pass
 
 # %%
